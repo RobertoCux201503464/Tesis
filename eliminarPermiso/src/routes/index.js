7719fd8e6d89d633708057bd179d158dec0b8c00
Roberto Cux <roberto.cux@gmail.com>
2021-04-24 16:27:17 -0600

    Eliminar Permiso
    Se crea funcion para eliminar permiso

---
+++ eliminarPermiso/src/routes/index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+const cors = require("cors");
+let bodyParser = require("body-parser");
+let urlencodedParser = bodyParser.json({ extended: false });
+const DataBaseHandler = require("../models/db");
+const handler = new DataBaseHandler();
+
+
+router.post("/eliminarPermiso", urlencodedParser, (req2, res2) => {
+  handler.getConnection().query("Call eliminarPermiso(\"" + req2.body.usuario + "\","+req2.body.pantalla+");", (err, res) => {
+    if (err) {
+      res2.status(500).json({
+        codigo: 500,
+        Estado: false,
+        data: 0
+      });
+      return;
+    }
+
+    res2.json({
+      codigo: 200,
+      Estado: true,
+      data: res[0][0]
+    });
+
+  });
+});
+
+module.exports = router;
