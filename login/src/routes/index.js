142b180946e88fbedde44c8201f4e6562f71b032
Roberto Cux <roberto.cux@gmail.com>
2021-04-24 15:45:43 -0600

    Login
    Se creo funcion para login.

---
+++ login/src/routes/index.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+const cors = require("cors");
+let bodyParser = require("body-parser");
+let urlencodedParser = bodyParser.json({ extended: false });
+const DataBaseHandler = require("../models/db");
+const handler = new DataBaseHandler();
+
+
+router.post("/login",urlencodedParser,(req2,res2)=>{
+  handler.getConnection().query("Call Login(\""+req2.body.usuario+"\",\""+req2.body.contrasena+"\");", (err,res)=>{
+    if(err){
+       res2.status(500).json({
+         codigo: 500,
+         Estado: false,
+         data: 0
+       });
+        return;
+    }
+
+    var cuenta=res[0][0];
+    if(cuenta==0){
+      res2.json({
+        codigo: 500,
+        Estado: false,
+        data: 0
+      });
+    }else{
+      res2.json({
+        codigo:200,
+        Estado: true,
+        data: cuenta.resultado
+      });
+
+    }
+
+
+});
+});
+
+ module.exports = router;
