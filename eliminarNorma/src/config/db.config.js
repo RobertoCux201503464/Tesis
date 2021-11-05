17bb81291bb47512db61b477c7b5f7a54539a616
Roberto Cux <roberto.cux@gmail.com>
2021-11-04 21:16:58 -0600

    Actualizacion de base de datos

--- eliminarNorma/src/config/db.config.js
+++ eliminarNorma/src/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: process.env.MYSQL_HOST ? process.env.MYSQL_HOST : '34.70.230.105',
+    HOST: process.env.MYSQL_HOST ? process.env.MYSQL_HOST : '34.70.80.75',
     USER: process.env.MYSQL_USER ? process.env.MYSQL_USER : 'root',
-    PASSWORD: process.env.MYSQL_PASSWORD ? process.env.MYSQL_PASSWORD :'root',
+    PASSWORD: process.env.MYSQL_PASSWORD ? process.env.MYSQL_PASSWORD : 'root',
     DB: process.env.MYSQL_DATABASE ? process.env.MYSQL_DATABASE : 'Tesis',
     PORT: process.env.MYSQL_PORT ? process.env.MYSQL_PORT : 3306,
-  };
+};
